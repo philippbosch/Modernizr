1e142acae27dbb359b07e8a21df4fddf69a99da5
Philipp Bosch <hello@pb.io>
2011-12-01 22:09:15 +0100

    Detect Page Visibility API.

---
+++ feature-detects/pagevisibility.js
@@ -0,0 +1,4 @@
+Modernizr.addTest('pagevisibility', ('hidden' in document || 'webkitHidden' in document ||
+    'mozHidden' in document || 'msHidden' in document));
+
+// http://www.w3.org/TR/2011/WD-page-visibility-20110602/
